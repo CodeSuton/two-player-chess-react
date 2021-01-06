e9ef2269635935fcdc90d4c785a341a318c057d3
Henry Ly <henryly213@gmail.com>
2021-01-05 21:15:46 -0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import Chess from "chess.js"
+import ChessBoard from "chessboardjsx"
+
+const boardContainer = {
+  "margin-top": "2rem",
+  display: "flex",
+  justifyContent: "space-around",
+  alignItems: "center"
+};
+
 
 function App() {
+
+  const [fen, setFen] = useState("start")
+  let game = useRef(null);
+
+  useEffect(() => {
+     game.current = new Chess();
+  },[])
+
+  const onDrop = ({ sourceSquare, targetSquare }) => {
+    let move = game.current.move({
+      from: sourceSquare,
+      to: targetSquare,
+    })
+    if (move === null) return;
+    setFen(game.current.fen());
+  }
+
+  const resetGame = () => {
+    game.current.clear();
+    game.current.reset();
+    setFen("start")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        game.current && game.current.game_over() ?
+          (<>
+            <div style={{ textAlign: "center" }}>
+            <h1>Game Over</h1>
+            <button onClick={resetGame}>Play Again</button>
+            </div>
+          </>)
+          : <span></span>
+      }
+      <div className="App" style={boardContainer}>
+        <ChessBoard position={fen} onDrop={onDrop}/>
+      </div>
+      </>
   );
 }
+
+
 
 export default App;
